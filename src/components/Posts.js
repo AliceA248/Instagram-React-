5f44d770102b4600a4e0b84bad184e335a91ed53
Alice Almeida <alice.pereira13@gmail.com>
2022-11-20 02:32:13 -0300

    componentizacao posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,76 +1,62 @@
-export default function Posts() {
+
+export default function Posts(){
     return (
         <div class="posts">
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="assets/img/meowed.svg" />
-                        meowed
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="assets/img/gato-telefone.svg" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="assets/img/respondeai.svg" />
-                        <div class="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="assets/img/barked.svg" />
-                        barked
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="assets/img/dog.svg" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-                    <div class="curtidas">
-                        <img src="assets/img/adorable_animals.svg" />
-                        <div class="texto">
-                            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Post 
+            imagem="/assets/img/meowed.svg"
+            textos="barked"
+            imagemMaior="/assets/img/gato-telefone.svg"
+            imagemMedia="/assets/img/respondeai.svg"
+            texto="respondeai"
+            />
+             <Post
+            imagem="assets/img/barked.svg"
+            textos="meowed"
+            imagemMaior="assets/img/dog.svg"
+            imagemMedia="assets/img/adorable_animals.svg"
+            texto="adorable_animals"
+            />
+            
         </div>
     )
 }
+
+
+
+function Post(a) {
+    return (
+        <div class="post">
+            <div class="topo">
+                <div class="usuario">
+                    <img src={a.imagem} />
+                    {a.textos}
+                </div>
+                <div class="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+            <div class="conteudo">
+                <img src={a.imagemMaior}/>
+            </div>
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src={a.imagemMedia} />
+                    Curtido por <strong> {a.texto} </strong> e <strong>outras 101.523 pessoas</strong>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
